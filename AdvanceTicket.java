47bfef6285fcf496cb7da4a1ba9dc3b7b6ec2ef2
SabraMGrace <sabramgrace@gmail.com>
2019-12-02 00:55:21 -0800

    Update AdvanceTicket.java

--- AdvanceTicket.java
+++ AdvanceTicket.java
@@ -4,3 +4,31 @@
 // Program: ticketPrinter
 // By: Sabra Grace
 //*******************************************************************
+package ticketPrinter;
+
+public class AdvanceTicket extends TicketPrinter {
+	  private static Object ticketNumber;
+	private int daysAhead;
+	public double ticketPrice;
+
+	  public AdvanceTicket(int number, int daysAhead) {
+	    super(ticketNumber);
+	    this.daysAhead = daysAhead;
+	  }
+
+	  public double getPrice() {
+	    if (daysAhead >= 10) {
+	      return 30.00;
+	    } else {
+	      return 40.00;
+	    }
+	  }
+
+	  public String toString() {
+	    if (daysAhead >= 10) {
+	      return super.toString() + " (" + this.daysAhead + " days ahead, you got a great deal!)";
+	    } else {
+	      return super.toString() + " (" + this.daysAhead + " days ahead, you could have saved a bit more)";
+	    }
+	  }
+	}
